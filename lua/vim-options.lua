5398e669776319385968d954b2e602c09ffa70fc
elnapoli <baldomero.napoli@gmail.com>
2024-03-27 16:20:07 -0500

    first commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,19 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+vim.g.background = "light"
+
+vim.opt.swapfile = false
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number =true
+
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+
+
