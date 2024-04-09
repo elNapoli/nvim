98e12275870e1a03d7c9a44ef17b5c5c5b1ab3a3
elnapoli <baldomero.napoli@gmail.com>
2024-04-09 11:38:50 -0500

    nuevos cambiso

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -24,7 +24,7 @@ return {
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<C-p>", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+            vim.keymap.set("n", "<leader><leader>", builtin.current_buffer_fuzzy_find, {})
             vim.keymap.set("n", "<leader>b", builtin.buffers, {})
             require("telescope").load_extension("ui-select")
         end,
