ba26f8754fd36b1356c29f2a1d4b7e432c8737f8
elnapoli <baldomero.napoli@gmail.com>
2024-04-10 21:50:49 -0500

    fix lazygit
    fix lazy git

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,24 +1,19 @@
 return {
-	"kdheepak/lazygit.nvim",
-	config = function()
-		local lazygit = require("lazy").setup({
-			{
-				cmd = {
-					"LazyGit",
-					"LazyGitConfig",
-					"LazyGitCurrentFile",
-					"LazyGitFilter",
-					"LazyGitFilterCurrentFile",
-				},
-				-- optional for floating window border decoration
-				dependencies = {
-					"nvim-lua/plenary.nvim",
-				},
-				-- setting the keybinding for LazyGit with 'keys' is recommended in
-				-- order to load the plugin when the command is run for the first time
-			},
-		})
-
-		vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>", { desc = "GIT UI " })
-	end,
+    "kdheepak/lazygit.nvim",
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
 }
